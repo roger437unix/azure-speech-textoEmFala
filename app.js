82adc646a2f0f0e99783175430d66355ac995d73
roger437unix <33252885+roger437unix@users.noreply.github.com>
2023-08-01 14:48:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+async function textoParaAudio  (texto) {
+    const SPEECH_KEY='d7d18aa8f319462a883b0464b0f7f0ad'
+    const SPEECH_REGION='eastus'
+    const url = `https://${SPEECH_REGION}.tts.speech.microsoft.com/cognitiveservices/v1`
+
+
+    const data = `
+        <speak version='1.0' xml:lang='pt-BR'>
+            <voice xml:lang='pt-BR' xml:gender='Female' name='pt-BR-GiovannaNeural'>
+                ${texto}
+            </voice>
+        </speak>
+    `
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Ocp-Apim-Subscription-Key': SPEECH_KEY,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'audio-16khz-128kbitrate-mono-mp3'
+          },
+        body: data,
+
+    }
+    
+    const response  = await fetch(url, options)
+    const datablob = await response.blob()
+
+    return datablob
+
+
+}
+
+const playAudio = async () => {
+
+    const audioPlayer = document.getElementById('player');
+    const texto = document.getElementById('texto').value
+
+
+    const blobAudio = await textoParaAudio(texto)
+
+    const objectURL = URL.createObjectURL(blobAudio);
+    audioPlayer.src = objectURL;
+    
+
+}
+
+document.getElementById('converter')
+        .addEventListener('click', () => playAudio())
